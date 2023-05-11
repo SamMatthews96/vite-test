d1d5c7abbe9720cfe50b72c61f640d5662c204a4
SamMatthews96 <sam-matthews@hotmail.co.uk>
2023-05-11 12:46:08 +0100

    removed images for testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import './style.sass'
 
@@ -11,10 +9,10 @@ function App() {
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <p>image</p>
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <p>image</p>
         </a>
       </div>
       <h1>Vite + React</h1>
