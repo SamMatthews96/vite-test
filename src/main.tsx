5ffac92c068303793fe7ea05c45cb780f2762dbc
SamMatthews96 <sam-matthews@hotmail.co.uk>
2023-05-16 11:58:00 +0100

    added css, removed redundant

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 
 const body = document.getElementsByTagName('body')[0]
-console.log('body')
-console.log(body)
-
 const div = document.createElement('div')
 body.appendChild(div)
 
